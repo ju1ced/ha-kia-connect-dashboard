83fba1d11dbf11dc6044b7347d1ed2af3ad3340c
Joost Billiet <joost@billietnet.be>
2026-08-06 15:44:27 +0200

    Prepare v2.6.0 release

--- ha-kia-connect-dashboard.js
+++ ha-kia-connect-dashboard.js
@@ -1,4 +1,4 @@
-const KIA_DASHBOARD_CARD_VERSION = "2.5.1";
+const KIA_DASHBOARD_CARD_VERSION = "2.6.0";
 const KIA_DASHBOARD_NL = {
   "AC charge target": "AC-laaddoel",
   "AC charging limit": "AC-laadlimiet",
